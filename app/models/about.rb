db742e1dcea1071f51a8797221cd6ce8f5c9c733
Anh Du Lio <lio.anhdu@gmail.com>
2017-07-05 15:21:43 +0700

    extend spec to about/blog

--- app/models/about.rb
+++ app/models/about.rb
@@ -3,5 +3,6 @@ class About < Content
   #FriendlyId for SEO URL
   extend FriendlyId
   content_attr :main_image, :string
+  content_attr :spec, :string
 
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,7 @@
 class Blog < Content
   #FriendlyId for SEO URL
   extend FriendlyId
+  content_attr :spec, :string
 
   content_attr :main_image, :string
 end
